ebf14f60963f15a21cb69a4b3cab1fbd24da4be2
DuongTruong <truongdinhdongduong@gmail.com>
2025-05-29 14:25:00 +0700

    feat: delete user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -47,7 +47,10 @@ export class UsersService {
       { _id: updateUserDto._id }, {... updateUserDto});
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return 'User not found';
+    }
+      return this.userModel.deleteOne({ _id: id });
   }
 }
