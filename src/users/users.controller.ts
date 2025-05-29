b63c8314cc40481a27b19b173fd6f5e64415182d
DuongTruong <truongdinhdongduong@gmail.com>
2025-05-29 16:35:34 +0700

    chore: test guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { TestGuard } from './test.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Get()
+  @UseGuards(TestGuard)
   findAll() {
     return this.usersService.findAll();
   }
