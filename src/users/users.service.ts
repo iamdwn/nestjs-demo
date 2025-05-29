49d42e4810ea4a76aef8f24c570d0814ee6ff97f
DuongTruong <truongdinhdongduong@gmail.com>
2025-05-29 14:10:30 +0700

    feat: get user by id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './schemas/user.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import {genSaltSync, hashSync} from 'bcryptjs';
+import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
 
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<User>) { }
 
   hashPassword = (password: string) => {
     const salt = genSaltSync(10);
@@ -18,7 +18,7 @@ export class UsersService {
   }
 
   async create(createUserDto: CreateUserDto) {
-  // async create(email: string, password: string, name?: string) {
+    // async create(email: string, password: string, name?: string) {
     const hashPassword = this.hashPassword(createUserDto.password);
 
     let user = await this.userModel.create({
@@ -35,8 +35,11 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return 'User not found';
+    }
+      return this.userModel.findOne({ _id: id });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
