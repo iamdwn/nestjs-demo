82882f60d3af0aeb0fcca7f86234863013b33c93
DuongTruong <truongdinhdongduong@gmail.com>
2025-07-08 06:14:25 +0700

    feat: update user schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,14 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsObject, IsString, Max, MaxLength, MinLength, Validate, ValidateNested } from 'class-validator';
+import mongoose, { Types } from 'mongoose';
+import { Type } from 'class-transformer';
+
+class Company {
+    @IsNotEmpty()
+    _id: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty()
+    name: string;
+}
 
 export class CreateUserDto {
     @IsEmail({}, { message: 'Email must be a valid email address', })
@@ -8,6 +18,24 @@ export class CreateUserDto {
     @IsNotEmpty({ message: 'Password is required', })
     password: string;
 
+    @IsNotEmpty({ message: 'Name is required', })
     name: string;
+
+    @IsNotEmpty({ message: 'Age is required', })
+    age: number;
+
+    @IsNotEmpty({ message: 'Gender is required', })
+    gender: number;
+
+    @IsNotEmpty({ message: 'Address is required', })
     address: string;
+
+    @IsNotEmpty({ message: 'Role is required', })
+    role: string;
+
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(() => Company)
+    company!: Company;
 }
