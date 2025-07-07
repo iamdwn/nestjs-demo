b761fe64ee6d9b96c7183737285a28724b0a991a
DuongTruong <truongdinhdongduong@gmail.com>
2025-07-08 05:31:10 +0700

    feat: crud company

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -10,8 +10,8 @@ export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) { }
 
   @Post()
-  create(@Body() createCompanyDto: CreateCompanyDto, @User() user : IUser) {
-    return this.companiesService.create(createCompanyDto);
+  create(@Body() createCompanyDto: CreateCompanyDto, @User() user: IUser) {
+    return this.companiesService.create(createCompanyDto, user);
   }
 
   @Get()
@@ -21,16 +21,16 @@ export class CompaniesController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.companiesService.findOne(+id);
+    return this.companiesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
-    return this.companiesService.update(+id, updateCompanyDto);
+  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto, @User() user: IUser) {
+    return this.companiesService.update(id, updateCompanyDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.companiesService.remove(+id);
+  remove(@Param('id') id: string, @User() user : IUser) {
+    return this.companiesService.remove(id, user);
   }
 }
