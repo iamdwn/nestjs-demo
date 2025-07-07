db43fad44729883b4fd9daf42bf12bb1f2a8d0bd
DuongTruong <truongdinhdongduong@gmail.com>
2025-07-08 05:41:09 +0700

    feat: add pagination

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
@@ -15,8 +15,8 @@ export class CompaniesController {
   }
 
   @Get()
-  findAll() {
-    return this.companiesService.findAll();
+  findAll(@Query("page") currentPage: string, @Query("limit") limit: number, @Query() qs: string) {
+    return this.companiesService.findAll(+currentPage, limit, qs);
   }
 
   @Get(':id')
@@ -30,7 +30,7 @@ export class CompaniesController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string, @User() user : IUser) {
+  remove(@Param('id') id: string, @User() user: IUser) {
     return this.companiesService.remove(id, user);
   }
 }
