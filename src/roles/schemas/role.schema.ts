7d2fc9cc270df44505b69dadde60b886692bf987
DuongTruong <truongdinhdongduong@gmail.com>
2025-07-08 07:47:55 +0700

    feat: permission and role

---
+++ src/roles/schemas/role.schema.ts
@@ -0,0 +1,59 @@
+import { Permission } from '@/permissions/schemas/permission.schema';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { timestamp } from 'rxjs';
+
+export type RoleDocument = HydratedDocument<Role>;
+
+@Schema({ timestamps: true })
+export class Role {
+    @Prop()
+    name: string;
+
+    @Prop()
+    description: string;
+
+    @Prop()
+    isActive: boolean;
+
+    @Prop({ type: Object })
+    company: {
+        _id: mongoose.Schema.Types.ObjectId;
+        email: string;
+    }
+
+    @Prop({type: [mongoose.Schema.Types.ObjectId], ref: Permission.name})
+    permisisons: Permission
+
+    @Prop({ type: Object })
+    createdBy: {
+        _id: mongoose.Schema.Types.ObjectId;
+        email: string;
+    };
+
+    @Prop({ type: Object })
+    updatedBy: {
+        _id: mongoose.Schema.Types.ObjectId;
+        email: string;
+    };
+
+    @Prop({ type: Object })
+    deletedBy: {
+        _id: mongoose.Schema.Types.ObjectId;
+        email: string;
+    };
+
+    @Prop()
+    createdAt: Date;
+
+    @Prop()
+    updatedAt: Date;
+
+    @Prop()
+    isDeleted: boolean;
+
+    @Prop()
+    deletedAt: Date;
+}
+
+export const RoleSchema = SchemaFactory.createForClass(Role);
