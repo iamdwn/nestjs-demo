da4febd5ecb7a5c394dfe7f209dba0fa35aa4905
DuongTruong <truongdinhdongduong@gmail.com>
2025-07-08 07:11:09 +0700

    feat: upload files

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import {softDeletePlugin} from 'soft-delete-mongoose';
 import { CompaniesModule } from './companies/companies.module';
 import { JobsModule } from './jobs/jobs.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -32,7 +33,8 @@ import { JobsModule } from './jobs/jobs.module';
     StatelessModule,
     AuthModule,
     CompaniesModule,
-    JobsModule
+    JobsModule,
+    FilesModule
   ],
   controllers: [AppController],
   providers: [AppService,
