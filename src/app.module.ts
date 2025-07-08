099ff7d659b184f80146c66a29936b39e9f6eda0
DuongTruong <truongdinhdongduong@gmail.com>
2025-07-08 07:39:55 +0700

    feat: resumes

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import {softDeletePlugin} from 'soft-delete-mongoose';
 import { CompaniesModule } from './companies/companies.module';
 import { JobsModule } from './jobs/jobs.module';
 import { FilesModule } from './files/files.module';
+import { ResumesModule } from './resumes/resumes.module';
 
 @Module({
   imports: [
@@ -34,7 +35,8 @@ import { FilesModule } from './files/files.module';
     AuthModule,
     CompaniesModule,
     JobsModule,
-    FilesModule
+    FilesModule,
+    ResumesModule
   ],
   controllers: [AppController],
   providers: [AppService,
